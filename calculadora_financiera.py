cbf7e5f2826020be7895ce7c994b13c9f08502ef
a01710040 <a01710040@tec.mx>
2025-09-05 22:31:49 -0600

    Update calculadora_financiera.py

--- calculadora_financiera.py
+++ calculadora_financiera.py
@@ -1,13 +1,19 @@
 #Calculadora Financiera
 
-ingreso_total = float(input("Hola!Has iniciado la Calculadora Financiera, por favor dime cuánto dinero vas a ingresar:"))
+def pedir_ingresos():
+    ingreso_total = float(input("Hola!Has iniciado la Calculadora Financiera, por favor dime cuánto dinero vas a ingresar:"))
+    return ingreso_total
+
+def sumar_ingresos(ingreso_total):
+    ingreso2 = float(input("¿Tienes otro ingreso? (usa '0' en caso de no tener)"))
+    while ingreso2 > 0 :
+        ingreso_total = ingreso_total + ingreso2
+        print("Has ingresado:$", ingreso_total)
+        ingreso2 = float(input("¿Tienes otro ingreso? (usa '0' en caso de no tener)"))
+    return ingreso_total
 
 
-ingreso2 = float(input("¿Tienes otro ingreso? (usa ¨0¨ en caso de no tener)"))
+ingreso_total = pedir_ingresos()
+ingreso_total = sumar_ingresos(ingreso_total)
 
-while ingreso2 > 0 :
-    ingreso_total = ingreso_total + ingreso2
-    print("Has ingresado:$", ingreso_total)
-    ingreso2 = float(input("¿Tienes otro ingreso? (usa ¨0¨ en caso de no tener)"))
-  
 print("Fin del programa, tus ingresos totales fueron de: $",ingreso_total)
