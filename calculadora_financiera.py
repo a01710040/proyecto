baf06e05e59847d6c14937ab04170b29214bb381
a01710040 <a01710040@tec.mx>
2025-10-09 08:57:53 -0600

    Update calculadora_financiera.py

--- calculadora_financiera.py
+++ calculadora_financiera.py
@@ -1,8 +1,14 @@
 #Calculadora Financiera
 
+lista_ingresos = []
+lista_egresos = []
+
 def sumar_ingresos(ingreso_total):
     ingreso2 = float(input("Introduce un ingreso (usa '0' para terminar): "))
     while ingreso2 > 0:
+        
+        lista_ingresos.append(ingreso2)
+      
         ingreso_total = ingreso_total + ingreso2
         print("El total de ingresos ahora es: $", ingreso_total)
         ingreso2 = float(input("Introduce otro ingreso (usa '0' para terminar): "))
@@ -11,6 +17,9 @@ def sumar_ingresos(ingreso_total):
 def sumar_egresos(egreso_total):
     egreso2 = float(input("Introduce un egreso/gasto (usa '0' para terminar): "))
     while egreso2 > 0:
+        
+        lista_egresos.append(egreso2)
+        
         egreso_total = egreso_total + egreso2
         print("El total de egresos ahora es: $", egreso_total)
         egreso2 = float(input("Introduce otro egreso (usa '0' para terminar): "))
@@ -34,4 +43,7 @@ while menu > 0:
     else:
         print("ERROR: Opción no válida")
 
+print("\n    Resumen Detallado    ")
+print("Registro de ingresos:", lista_ingresos)
+print("Registro de egresos:", lista_egresos)
 print("Fin del programa, tus ingresos totales fueron de: $", ingreso_total, "y tus egresos totales fueron de: $", egreso_total, "tu balance actual es de: $", (ingreso_total - egreso_total))
